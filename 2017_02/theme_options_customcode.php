0139c773659c12bba9ea8c9383c0ef41f58c4690
Tiago Tadeu <tiagotad@gmail.com>
2017-02-20 08:54:52 -0300

    acrecentar ./

---
+++ 2017_02/theme_options_customcode.php
@@ -0,0 +1,162 @@
+CUSTOM CSS
+
+.cb-body-light .cb-sidebar {background: #fff; padding: 0px; }
+
+.cb-module-header .cb-module-title:after {   height: 0px; }
+.cb-module-header .cb-module-title a, .cb-module-header .cb-module-title {border-bottom: 1px solid #000; }
+ .wp-caption-text {
+     font-size: 12px;
+    position: absolute;
+    right: 0;
+    bottom: 16px;
+    color: #fff;
+    background: rgba(0, 0, 0, 0.5);
+    padding: 5px 10px;
+    line-height: 1.2;
+}
+#cb-nav-bar .cb-main-nav>li>a {padding: 5px !important;}
+
+.cb-logo-nav-always-type {margin-top: -12px !important;}
+.cb-nav-bar-wrap  {padding-top: 10px;}
+.cb-module-header h2 {
+    font-size: 35px;
+}
+
+.cb-entry-content .alignnone .wp-caption-text { font-style: normal !important;}
+@media only screen and (min-width: 768px){
+    .cb-stuck #cb-nav-logo, #cb-nav-logo img {
+    max-width: 140px;
+}#cb-nav-bar .cb-main-nav #cb-nav-logo {
+    max-width: 140px;
+}
+#media-social {max-width: 520px;}
+.mashsb-count {    margin-top: 20px !important;}
+}
+@media only screen and (max-width: 800px) {
+    .cb-module-header h2 {
+    font-size: 20px;
+}
+}
+.publicidade_mobile {display:none;}
+@media screen and (max-width: 600px) {
+.publicidade_desktop {display:none;}
+#ig-barra2 {display:none;}
+.publicidade_mobile {display:inherit;}
+.publicidade_mobile { text-align: center;margin: 0 auto; width: 300px; }
+#barra-parceiros-container {display: none !important;}
+}
+
+.cb-fis .cb-fis-bg { opacity: 0.6};
+
+.cb-sidebar {padding: 0px !important;}
+
+
+
+CODE FOR HEAD
+
+<!-- Ig Cluster -->
+<script type="text/javascript" src="http://js.statig.com.br/pub/thirdPartyTags.js"></script>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5WGCRH"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5WGCRH');</script>
+<!-- End Google Tag Manager -->
+<!-- Aud -->
+<script type="text/javascript">
+var _gaq = _gaq || [];
+var pluginUrl = 
+ '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+_gaq.push(['_require', 'inpage_linkid', pluginUrl])
+_gaq.push(['_setAccount', 'UA-3531175-1']);
+_gaq.push(['_trackPageview']);
+_gaq.push(['ig._setAccount', 'UA-33571001-1']);
+_gaq.push(['ig._trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript';
+ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+})();
+var _comscore = _comscore || [];
+_comscore.push({ c1: "2", c2: "6987205" });
+(function() {
+var s = document.createElement("script"), el =
+document.getElementsByTagName("script")[0]; s.async = true;
+s.src = (document.location.protocol == "https:" ? "https://sb" :
+"http://b")
++ ".scorecardresearch.com/beacon.js";
+el.parentNode.insertBefore(s, el);
+})();
+</script>
+
+<!-- Google ADS -->
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3829748395962560",
+    enable_page_level_ads: true
+  });
+</script>
+
+
+
+CODE FOR <FOOTER> SECTION
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1717583441788705');
+fbq('track', "PageView");</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1717583441788705&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Facebook Pixel Code -->
+
+<!-- TailTarget Tag Manager TT-11475-8/CT-572 -->
+<script>
+    (function(i) {
+    var ts = document.createElement('script');
+    ts.type = 'text/javascript';
+    ts.async = true;
+    ts.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'tags.t.tailtarget.com/t3m.js?i=' + i;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ts, s);
+    })('TT-11475-8/CT-572');
+</script>
+<!-- End TailTarget Tag Manager -->
+
+<!-- BEGIN SNIPPET -->
+<div class="YAD-0b33e2a9-50a8-322c-8fc0-3ac469ec2a85"></div>
+<script>
+    (function(a,b,c,d,e,f){a[d]||(a[d]=
+    function(){(a[d].q=a[d].q||[]).push([arguments,+new Date])});
+    e=b.createElement(c);f=b.getElementsByTagName(c)[0];
+    e.src='https://s.yimg.com/uq/syndication/yad.js';e.async=true;
+    f.parentNode.insertBefore(e,f)}(window,document,'script','yad'));
+    yad('0b33e2a9-50a8-322c-8fc0-3ac469ec2a85');
+</script>
+<!-- END SNIPPET -->
+
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-33571001-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
---
+++ 2017_02/widget_TopRosHeader.php
@@ -0,0 +1,164 @@
+TOP ROS HEADER
+
+<!-- Cabeçalho das Ads do El Hombre sequenciados por Home, Internas, Mobile -->
+<?php 
+$iphone = strpos($_SERVER['HTTP_USER_AGENT'],"iPhone");
+$ipad = strpos($_SERVER['HTTP_USER_AGENT'],"iPad");
+$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+$palmpre = strpos($_SERVER['HTTP_USER_AGENT'],"webOS");
+$berry = strpos($_SERVER['HTTP_USER_AGENT'],"BlackBerry");
+$ipod = strpos($_SERVER['HTTP_USER_AGENT'],"iPod");
+$symbian =  strpos($_SERVER['HTTP_USER_AGENT'],"Symbian");
+
+// Versao Dekstop
+if ($iphone || $ipad || $android || $palmpre || $ipod || $berry || $symbian == true) {
+ wp_reset_query(); 
+if (is_home() || is_front_page() == true) { ?>
+<!-- ELH Mobile Home -->
+<div class="publicidade_mobile">
+<style> @media screen and (max-width: 600px) {#cb-content {display: none;}} </style>
+<!-- /132061177/ELH_mobile_home_320x100_Rectangule1 -->
+<div id='div-gpt-ad-1482149881821-0' style='height:50px; width:320px;'>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1482149881821-0'); });
+</script>
+</div>
+</div>
+<?php } elseif (in_category(array('especiais','app-runtastic','doutor-grooming-by-dr-jones','doutor-grooming-by-dr-jones','melhor-ano-da-sua-vida','mens-market','preserv','lifestyle-by-dr-jones','easynvest','philips-body-grooming','25-blogs-de-moda-masculina','cuidados_pessoais','modamasculina','relogios','barba-brava','gillettenfl2017'))) {?>
+
+<?php } elseif (in_category('sexo')) {?>
+<div class="publicidade_mobile">
+<style> @media screen and (max-width: 600px) {.cb-category-top {display: none;}} </style>
+
+<!-- /132061177/ELH_mobile_18_320x50_Rectangule1 -->
+<div id='div-gpt-ad-1482345705990-5' style='height:50px; width:320px;'>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1482345705990-5'); });
+</script>
+</div>
+</div>
+<?php } elseif (in_category('musas')) {?>
+<div class="publicidade_mobile">
+<style> @media screen and (max-width: 600px) {.cb-category-top {display: none;}} </style>
+
+<!-- /132061177/ELH_mobile_18_320x50_Rectangule1 -->
+<div id='div-gpt-ad-1482345705990-5' style='height:50px; width:320px;'>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1482345705990-5'); });
+</script>
+</div>
+</div>
+
+<?php } else {?>
+<!-- ELH Mobile Interna -->
+<div class="publicidade_mobile">
+<!-- /132061177/ELH_mobile_internas_320x100_Rectangule1 -->
+<style> @media screen and (max-width: 600px) {.cb-category-top {display: none;}} </style>
+<div id='div-gpt-ad-1482150056663-0' style='height:50px; width:320px; margin-top: 2px; margin-bottom: 2px;'>
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1482150056663-0'); });
+</script>
+</div>
+</div>
+<?php } } else { if (is_home() || is_front_page()) { ?>
+
+<!-- ELH Home Padrao -->
+<div class="publicidade_desktop">
+<!-- /132061177/ELH_Home_950x250_Billboard1 -->
+<div id='div-gpt-ad-1477929494191-1' style="height:90px; width:728px; margin:0 auto;">
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1477929494191-1'); });
+</script>
+</div>
+</div>
+<?php } elseif (in_category('sexo')) {?>
+
+<!-- /132061177/ELH_desktop_18_950x250_Billboard1 -->
+<div id='div-gpt-ad-1482345705990-3' style="height:90px; width:728px; margin: 0 auto; ">
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1482345705990-3'); });
+</script>
+</div>
+<?php } elseif (in_category('musas')) {?>
+<!-- /132061177/ELH_desktop_18_950x250_Billboard1 -->
+<div id='div-gpt-ad-1482345705990-3' style="height:90px; width:728px; margin: 0 auto; ">
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1482345705990-3'); });
+</script>
+</div>
+
+<?php } elseif (is_category('especiais')) {?>
+<div class="publicidade_desktop">
+
+</div>
+<?php } elseif (in_category('philips-body-grooming')){?>
+<?php } elseif (in_category('app-runtastic')){?>
+<div class="publicidade_desktop" style=" width:728px; height: 90px; margin: 0 auto;">
+     <a href="http://elhomb.re/1Tf2zse"> <img src="http://www.elhombre.com.br/wp-content/uploads/2016/09/elh_appruntastic_728x90.jpg"></a>
+</div>
+<?php } elseif (in_category('bangood')){?>
+<?php } elseif (in_category('di-pollini')){?>
+<div class="publicidade_desktop" style=" width:728px; height: 90px; margin: 0 auto;">
+     <a href="http://www.dipollini.com.br/?utm_source=elhombre&amp;utm_medium=midia&amp;utm_campaign=elhombre"> <img src="http://www.elhombre.com.br/wp-content/uploads/2015/11/DP-super.jpg"></a>
+</div>
+<?php } elseif (in_category('dr-jones')){?>
+<!-- Especial sem segmentaca -->
+<a href="https://goo.gl/3N58Ex" target="_blank"><img class="alignnone size-full wp-image-100154" src="http://www.elhombre.com.br/wp-content/uploads/2016/03/jones.jpg" alt="jones" width="728" height="90" /></a>
+
+<?php } elseif (in_category('fitness-care')){?>
+<!-- Especial sem segmentaca -->
+<a href="http://www.runtastic.com/pt/results"> 
+<img src="http://www.elhombre.com.br/wp-content/uploads/2015/11/Lush-Banner-ElHombre-728x90-2015.11.19-GIF-3.gif">
+</a>
+
+
+<?php } elseif (in_category('melhor-ano-da-sua-vida')){?>
+<!-- Especial sem segmentaca -->
+<?php } elseif (in_category('grooming')){?>
+    <div class="publicidade_desktop">
+<!-- /132061177/ELH_internas_950x250_Billboard1 -->
+<div id='div-gpt-ad-1477929494191-4' style="height:90px; width:728px; margin: 0 auto; ">
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1477929494191-4'); });
+</script>
+</div>
+</div>
+    <?php } elseif (in_category('lifestyle')){?>
+      <div class="publicidade_desktop">
+<!-- /132061177/ELH_internas_950x250_Billboard1 -->
+<div id='div-gpt-ad-1477929494191-4' style="height:90px; width:728px; margin: 0 auto; ">
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1477929494191-4'); });
+</script>
+</div>
+</div>
+
+<?php } elseif (in_category('modamasculina')){?>
+<?php } elseif (in_category('lifestyle-by-dr-jones')){?>
+
+<?php } elseif (in_category('lush-motel')){?>
+<div class="publicidade_desktop" style=" width:728px; height: 90px; margin: 0 auto;">
+<a href="http://elhomb.re/1Tf2zse"> <img src="http://www.elhombre.com.br/wp-content/uploads/2016/09/elh_appruntastic_728x90.jpg"></a>
+</div>
+
+<?php } elseif (in_category('mens-market')){?>
+<?php } elseif (in_category('onix-jeans')){?>
+<?php } elseif (in_category('preserv')){?>
+<?php } elseif (in_category('ricardo-almeida')){?>
+<?php } elseif (in_category('reserva51')){?>
+<?php } elseif (in_category('shop4men')){?>
+<?php } elseif (in_category('tie')){?>
+<?php } else { ?>
+<!-- Interna Dekstop -->
+<div class="publicidade_desktop">
+<!-- /132061177/ELH_internas_950x250_Billboard1 -->
+<div id='div-gpt-ad-1477929494191-4' style="height:90px; width:728px; margin: 0 auto; ">
+<script>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1477929494191-4'); });
+</script>
+</div>
+</div>
+
+<?php } } ?>
+
+ <!-- Fim sequencia  -->
